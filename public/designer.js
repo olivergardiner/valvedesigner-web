df8d6e372f3151648c97f2113c1c4a396c1a4ce3
olivergardiner <olivergardiner@whitecottage.org.uk>
2024-07-24 13:02:09 +0100

    Initial commit - working triodecc/12ax7

---
+++ public/designer.js
@@ -0,0 +1,269 @@
+/**
+ * Code to model various tube amplifier circuits using a variety of tube models
+ */
+
+function intersection(series1, series2) {
+	for (let i = 0; i < series1.length - 1; i++) {
+	    let p1 = series1[i];
+	    let p2 = series1[i + 1];
+	    for (let j = 0; j < series2.length - 1; j++) {
+	        let p3 = series2[j];
+	        let p4 = series2[j + 1];
+	        let a1 = p2.y - p1.y;
+	        let b1 = p1.x - p2.x;
+	        let c1 = a1 * p1.x + b1 * p1.y;
+	        let a2 = p4.y - p3.y;
+	        let b2 = p3.x - p4.x;
+	        let c2 = a2 * p3.x + b2 * p3.y;
+	        let d = a1 * b2 - a2 * b1;
+	        if (d != 0.0) {
+	            let x = (b2 * c1 - b1 * c2) / d;
+	            let y = (a1 * c2 - a2 * c1) / d;
+
+	            let onLine1 = x > Math.min(p1.x, p2.x) && x < Math.max(p1.x, p2.x) && y > Math.min(p1.y, p2.y) && y < Math.max(p1.y, p2.y);
+	            let onLine2 = x > Math.min(p3.x, p4.x) && x < Math.max(p3.x, p4.x) && y > Math.min(p3.y, p4.y) && y < Math.max(p3.y, p4.y);
+
+	            if (onLine1 && onLine2) {
+	                return {point: {x: x, y: y}, i: i ,j: j};
+	            }
+	        }
+	    }
+	}
+
+	return {point: {x: 0, y: 0}, i: -1 , j: -1};
+}
+
+class Circuit {
+	constructor(ctx) {
+		this.ctx = ctx;
+		this.chart = null;
+		this.parameters = {
+			nFields: 0,
+			field: [],
+			nValues: 0,
+			value: []
+		};
+	}
+	
+	loadDevice(device) {
+		let thisCircuit = this;
+		jQuery.ajax({
+		    type: "get",
+		    url: "devices/" + device,
+		    contentType: 'application/json'
+		}).then(function(data, status, jqxhr){
+		    if (data != null) {
+				thisCircuit.setDevice(data);
+		    }
+		});
+	}
+	
+	configureParameters() {
+		for (let i = 0; i < 8; i++) {
+			if (i < this.parameters.nFields) {
+				$('#fLabel' + (i + 1)).text(this.parameters.field[i].name);
+				$('#field' + (i + 1)).val(this.parameters.field[i].value);
+				$('#fLabel' + (i + 1)).show();
+				$('#field' + (i + 1)).show();
+			} else {
+				$('#fLabel' + (i + 1)).hide();
+				$('#field' + (i + 1)).hide();
+			}
+		}
+		
+		for (let i = 0; i < 8; i++) {
+			if (i < this.parameters.nValues) {
+				$('#vLabel' + (i + 1)).text(this.parameters.value[i]);
+				$('#vLabel' + (i + 1)).show();
+				$('#value' + (i + 1)).show();
+			} else {
+				$('#vLabel' + (i + 1)).hide();
+				$('#value' + (i + 1)).hide();
+			}
+		}
+	}
+	
+	updateParameters() {		
+		for (let i = 0; i < this.parameters.nFields; i++) {
+			this.parameters.field[i].value = parseFloat($('#field' + (i + 1)).val());
+			$('#field' + (i + 1)).text(this.parameters.field[i].value);
+		}
+		
+		this.showPlots();
+	}
+		
+	setDevice(device) {
+		this.device = new Device(structuredClone(device));
+
+		this.showPlots(); 
+	}
+	
+	showPoint(x, y, color, label = null) {
+		let point = { x: x, y: y };
+		let dataset = {
+			data: [point],
+			label: label,
+			borderColor: color,
+			pointBackgroundColor: color,
+			pointStyle: 'circle',
+			pointRadius: 5
+		};
+		
+		return dataset;
+	}
+	
+	showPlots() {
+		if (this.chart != null) {
+			this.chart.destroy();
+		}
+		
+		this.anodeCurves = [];
+		this.operatingPointCurves = [];
+		let datasets = [];
+
+		this.loadLines(datasets);
+		
+		this.anodePlot(datasets);
+		
+		this.drawChart(datasets);
+	}
+		
+	drawChart(datasets) {
+		let chart = {
+			type : 'line',
+			indexAxis : 'x',
+			data : { datasets: this.operatingPointCurves.concat(this.anodeCurves) },
+			options : {
+				animation: false,
+				responsive : true,
+				maintainAspectRatio:false,
+				stacked : false,
+				hoverMode : 'index',
+				elements: {
+					point: {
+						pointStyle: false
+					}
+				},
+				plugins: {
+					title : {
+						display : true,
+						text : 'Anode Characteristic Graph',
+					},
+					legend : {
+						display : false,
+						labels : {
+							fontColor : 'rgb(255, 99, 132)'
+						}
+					}
+				},
+				scales : {
+					x : {
+						type : 'linear',
+						max : this.device.definition.vaMax,
+						beginAtZero : true
+					},
+					y : {
+						max : this.device.definition.iaMax,
+						beginAtZero : true
+					}
+				}
+			}
+		};
+	
+		this.chart = new Chart(this.ctx, chart);
+	}
+	
+	anodePlot(datasets) {
+	}
+	
+	loadLines(datasets) {
+	}
+}
+
+class Device {
+	constructor(definition) {
+		this.definition = definition;
+		
+		if (this.definition.model.device === "triode") {
+			if (definition.model.type === "cohenHelie") {
+				this.model = new CohenHelieTriode(definition.model);
+			}
+		}
+	}
+
+	getDefinition()	{
+		return this.definition;
+	}
+
+	getModel()	{
+		return this.model;
+	}
+}
+
+class Model {
+	constructor(model) {
+		this.model = model;
+	}
+	
+	anodeCurrent(anodeVoltage, gridVoltage, screenVoltage = 0, secondaryEmission = true) {
+		return 0;
+	}
+	
+	anodeVoltage(cathodeCurrent, gridVoltage, screenVoltage = 0, secondaryEmission = true) {
+		return 0;
+	}
+
+	anodeVoltage(ia, vg1, vg2 = 0, secondaryEmission = true) {
+		let va = 100.0;
+		let tolerance = 1.2;
+	
+		let iaTest = 1000.0 * this.anodeCurrent(va, vg1, vg2, secondaryEmission);
+		let gradient = 100.0 * (1000.0 * this.anodeCurrent(va + 0.01, vg1, vg2, secondaryEmission) - iaTest);
+		let iaErr = ia - iaTest;
+	
+		let count = 0;
+		while (Math.abs(iaErr) > 0.005 && count++ < 1000) {
+		    if (gradient != 0.0) {
+		        let vaNext = va + iaErr / gradient;
+		        if (vaNext < 0.0) {
+		            vaNext = 0.0;
+		        }
+		        if (vaNext < va / tolerance) { // use the gradient but limit step to tolerance
+		            vaNext = va / tolerance;
+		        }
+		        if (vaNext > tolerance * va) { // use the gradient but limit step to tolerance
+		            vaNext = tolerance * va;
+		        }
+		        va = vaNext;
+		    } else {
+		        break;
+		    }
+		    iaTest = 1000.0 * this.anodeCurrent(va, vg1, vg2, secondaryEmission);
+		    gradient = 100.0 * (1000.0 * this.anodeCurrent(va + 0.01, vg1, vg2, secondaryEmission) - iaTest);
+		    iaErr = ia - iaTest;
+		}
+	
+		return va;
+	}
+}
+
+class CohenHelieTriode extends Model {
+	constructor(model) {
+		super(model);
+	}
+
+	anodeCurrent(anodeVoltage, gridVoltage, screenVoltage = 0, secondaryEmission = true) {
+		return this.cohenHelieCurrent(anodeVoltage, gridVoltage);
+	}
+	
+	cohenHelieCurrent(anodeVoltage, gridVoltage) {
+		return this.cohenHelieEpk(anodeVoltage, gridVoltage) / this.model.kg1;
+	}
+	
+	cohenHelieEpk(voltage, gridVoltage) {
+		let f = Math.sqrt(this.model.kvb + voltage * this.model.kvb1 + voltage * voltage);
+		let y = this.model.kp * (1.0 / this.model.mu + (gridVoltage + this.model.vct) / f);
+		let ep = voltage / this.model.kp * Math.log(1.0 + Math.exp(y));
+		return Math.pow(ep, this.model.x);
+	}
+}
---
+++ public/triodecc.js
@@ -0,0 +1,121 @@
+class TriodeCC extends Circuit {
+	constructor(ctx) {
+		super(ctx);
+		
+		this.parameters = {
+			nFields: 4,
+			field: [
+				{ name: 'Supply voltage', value: 300 },
+				{ name: 'Cathode resistor Rk', value: 1000 },
+				{ name: 'Anode resistor Ra', value: 100000 },
+				{ name: 'Load impedance Rl', value: 1000000 }
+			],
+			nValues: 5,
+			value: [
+				'Bias point Vk (V)',
+				'Anode voltage (V)',
+				'Anode current (mA)',
+				'Gain',
+				'Gain (bypassed)'
+			]
+		};
+	}
+	
+	anodePlot() {
+		let vg1Step = this.device.definition.vg1Step;
+		let vg1Max = this.device.definition.vg1Max;
+		let vg1 = 0.0;
+		let k = -1;
+		let i = 0;
+		while (vg1 < vg1Max + 0.001) {
+			if (Math.abs(this.vk - vg1) < vg1Step) {
+				if (k == -1) {
+					k = i;
+				}
+			}
+			let anodeCurve = [];
+		    for (let i = 1; i < 101; i++) {
+		        let va = this.device.definition.vaMax * i / 100.0;
+				let ia = this.device.model.anodeCurrent(va, -vg1) * 1000.0;
+		        anodeCurve.push({x: va, y: ia});
+		    }
+			
+			let dataset = {
+				data: anodeCurve,
+				borderColor: 'rgba(0, 0, 0, 1)'
+			};
+			
+			this.anodeCurves.push(dataset)
+		    vg1 += vg1Step;
+			i++;
+		}
+
+		let ra = this.parameters.field[2].value;
+		let rl = this.parameters.field[3].value;
+		let scale = rl / (ra + rl);
+		let gainPoint = intersection(this.anodeCurves[k].data, this.operatingPointCurves[1].data);
+		let deltaVa = this.va - gainPoint.point.x;
+		let deltaVg1 = this.vk - k * vg1Step;
+		let gain = scale * deltaVa / deltaVg1;
+		$('#value5').text(gain.toFixed(3));
+	}
+	
+	loadLines() {
+		let anodeLoadLine = [];
+		
+		let vb = this.parameters.field[0].value;
+		let rk = this.parameters.field[1].value;
+		let ra = this.parameters.field[2].value;
+		let rl = this.parameters.field[3].value;
+		let vgMax = this.device.definition.vg1Max;
+		let mu = this.device.model.model.mu;
+		
+		let ia = vb / (ra + rk) * 1000.0;
+		anodeLoadLine.push({x: 0, y: ia});
+		anodeLoadLine.push({x: vb, y: 0});
+
+		let aLLDataset = {
+			data: anodeLoadLine,
+			borderColor: 'rgba(127, 0, 0, 1)'
+		};
+				
+		let cathodeLoadLine = [];
+		for (let j = 1; j < 101; j++) {
+		    let vg = vgMax * j / 100.0;
+		    let ia = vg * 1000.0 / rk;
+		    let va = this.device.model.anodeVoltage(ia, -vg);
+			cathodeLoadLine.push({x: va, y: ia});
+		}
+
+		let cLLDataset = {
+			data: cathodeLoadLine,
+			borderColor: 'rgba(0, 0, 127, 1)'
+		};
+				
+		let operatingPoint = intersection(anodeLoadLine, cathodeLoadLine);
+		
+		this.operatingPointCurves.push(this.showPoint(operatingPoint.point.x, operatingPoint.point.y, 'rgba(0, 255, 0, 1)', 'Operating point'));
+		this.operatingPointCurves.push(aLLDataset);
+		this.operatingPointCurves.push(cLLDataset);
+		
+		this.va = operatingPoint.point.x;
+		this.ia = operatingPoint.point.y;
+		
+		this.vk = this.ia * rk / 1000.0;
+		
+		let ia1 = this.device.model.anodeCurrent(vb, -this.vk);
+		let ia2 = this.device.model.anodeCurrent(vb - 10.0, -this.vk);
+		let ar = 10.0 / (ia1 - ia2);
+
+		let re = ra * rl / (ra + rl);
+		let ark = ar + rk * (mu + 1.0);
+		let gain = mu * re / (re + ark);
+		let gainBP = mu * re / (re + ar);
+		
+		$('#value1').text(this.vk.toFixed(3));
+    	$('#value2').text(this.va.toFixed(3));
+		$('#value3').text(this.ia.toFixed(3));
+		$('#value4').text(gain.toFixed(3));
+		$('#value5').text(gainBP.toFixed(3));
+	}
+}
